a271030b9d4ae8b4c212a6bf3e3ad1cb255a6db6
Lukas Wessel <l.wessel@krz.de>
2023-08-19 10:43:24 +0200

    updated gitignore.... correctly

--- SchweinchendatenOcrToTable/Form1.Designer.cs
+++ SchweinchendatenOcrToTable/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(536, 146);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(158, 143);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Einscannen";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(252, 146);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(164, 138);
+            this.btnUpload.TabIndex = 1;
+            this.btnUpload.Text = "button1";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btnProcess;
+        private Button btnUpload;
     }
 }
--- SchweinchendatenOcrToTable/Form1.cs
+++ SchweinchendatenOcrToTable/Form1.cs
@@ -1,3 +1,5 @@
+using Tesseract;
+
 namespace SchweinchendatenOcrToTable
 {
     public partial class Form1 : Form
@@ -5,6 +7,51 @@ namespace SchweinchendatenOcrToTable
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string _imagePath = string.Empty;
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Image Files|*.jpg;*.png;*.bmp", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    _imagePath = ofd.FileName;
+                }
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_imagePath))
+            {
+                MessageBox.Show("Please upload an image first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(_imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+
+                        // You might need more sophisticated logic for parsing table and fractional numbers
+                        string[] lines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                        using (StreamWriter sw = new StreamWriter("output.csv"))
+                        {
+                            foreach (var line in lines)
+                            {
+                                sw.WriteLine(line.Replace("\t", ","));
+                            }
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show("Processing complete!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
